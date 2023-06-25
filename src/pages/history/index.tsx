d209eef3afb17b894aa6e1189040df4066997bbc
cchichen <oscers1001dennis@gmail.com>
2023-06-25 16:37:17 -0500

    Tabular display using shadcn/ui

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,16 +1,56 @@
 import type { InferGetStaticPropsType, GetStaticProps } from "next";
 import path from "path";
 import fs from "fs/promises";
-import QuoteList, {
-  type QuoteProps,
-  type QuotesListProps,
-} from "~/Components/QuoteList";
+import { type QuoteProps, type QuotesListProps } from "~/Components/QuoteList";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const index = ({
   quotings,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return <QuoteList quotes={quotings} />;
+  return (
+    <Table>
+      <TableCaption
+        className="mb-4 text-xl">
+        A list of your Past Quotes
+      </TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID</TableHead>
+          <TableHead>Gallons</TableHead>
+          <TableHead>Address</TableHead>
+          <TableHead>DateDelivered</TableHead>
+          <TableHead>Suggested Price</TableHead>
+          <TableHead className="text-center">Total Due Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {quotings.map((quote) => (
+          <TableRow key={quote.id}>
+            <TableCell className="text-center font-medium">
+              {quote.id}
+            </TableCell>
+            <TableCell className="text-center">{quote.gallonsreq}</TableCell>
+            <TableCell className="text-center">{quote.address1}</TableCell>
+            <TableCell className="text-center">{quote.deliveryDate}</TableCell>
+            <TableCell className="text-center">
+              {quote.suggestedPrice}
+            </TableCell>
+            <TableCell className="text-center">{quote.totalDue}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
+
 export const getStaticProps: GetStaticProps<{
   quotings: QuoteProps[];
 }> = async () => {
